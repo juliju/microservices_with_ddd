be37397d0e151362c514781936b5f1d62d8a446b
KHA <kevin.hantzen@gmail.com>
2020-11-13 17:54:01 +0100

    complete Book class

--- bounded-contexts/java/src/main/java/p1/myshop/entities/Book.java
+++ bounded-contexts/java/src/main/java/p1/myshop/entities/Book.java
@@ -1,13 +1,25 @@
 package p1.myshop.entities;
 
 public class Book implements Item {
+    private ItemId itemId;
+    private String ean;
+
+    public Book(ItemId itemId, String ean) {
+        this.itemId = itemId;
+        this.ean = ean;
+    }
+
     @Override
     public ItemId itemId() {
-        return null;
+        return this.itemId;
     }
 
     @Override
     public double weight() {
         return 0;
+    }
+
+    public String getEan() {
+        return ean;
     }
 }
