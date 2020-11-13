40314604650e59a35d646483b71d5f65906e902d
KHA <kevin.hantzen@gmail.com>
2020-11-13 18:16:05 +0100

    move cart to entities package

--- bounded-contexts/java/src/main/java/p1/myshop/services/shoppingcart/ShoppingCartService.java
+++ bounded-contexts/java/src/main/java/p1/myshop/services/shoppingcart/ShoppingCartService.java
@@ -3,6 +3,6 @@ package p1.myshop.services.shoppingcart;
 import p1.myshop.entities.HasWeight;
 import p1.myshop.services.catalog.CatalogService;
 
-public class ShoppingCarService {
+public class ShoppingCartService {
 
 }
