be046a5194c91f0a12f328ccb03224eaffc7860c
KHA <kevin.hantzen@gmail.com>
2020-11-13 17:54:46 +0100

    complete Dvd class

--- bounded-contexts/java/src/main/java/p1/myshop/entities/Dvd.java
+++ bounded-contexts/java/src/main/java/p1/myshop/entities/Dvd.java
@@ -1,6 +1,14 @@
 package p1.myshop.entities;
 
 public class Dvd implements Item {
+    private ItemId itemId;
+    private String ean;
+
+    public Dvd(ItemId itemId, String ean) {
+        this.itemId = itemId;
+        this.ean = ean;
+    }
+
     @Override
     public ItemId itemId() {
         return null;
@@ -9,5 +17,9 @@ public class Dvd implements Item {
     @Override
     public double weight() {
         return 0;
+    }
+
+    public String getEan() {
+        return ean;
     }
 }
