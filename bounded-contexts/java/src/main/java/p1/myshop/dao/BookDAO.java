4fe9a7270ed2370b606765fa5736b9206723756f
KHA <kevin.hantzen@gmail.com>
2020-11-13 18:16:33 +0100

    dao can recover object by id

--- bounded-contexts/java/src/main/java/p1/myshop/dao/BookDAO.java
+++ bounded-contexts/java/src/main/java/p1/myshop/dao/BookDAO.java
@@ -1,4 +1,8 @@
 package p1.myshop.dao;
 
+import p1.myshop.entities.Book;
+import p1.myshop.entities.ItemId;
+
 public interface BookDAO {
+    Book find(ItemId itemId);
 }
--- bounded-contexts/java/src/main/java/p1/myshop/dao/DvdDAO.java
+++ bounded-contexts/java/src/main/java/p1/myshop/dao/DvdDAO.java
@@ -1,4 +1,8 @@
 package p1.myshop.dao;
 
+import p1.myshop.entities.Dvd;
+import p1.myshop.entities.ItemId;
+
 public interface DvdDAO {
+    Dvd find(ItemId itemId);
 }
